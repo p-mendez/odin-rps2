6b20fdb23a7cc0f83636be2a108c40a97c78fd94
p-mendez <139092055+p-mendez@users.noreply.github.com>
2023-12-20 17:50:25 -0500

    Add playRound logic

--- script.js
+++ script.js
@@ -17,4 +17,55 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) { }
+/* Return a string that declares the winner of the round 
+ * like so: "You Lose! Paper beats Rock".
+ *  
+ * Algorithm: 
+ * If both selections are the same, it's a tie. 
+ * Otherwise, go through every remaining combination,
+ * assigning either "You Lose" or "You win"
+ */
+function playRound(playerSelection, computerSelection) {
+    // tied
+    if (playerSelection == computerSelection) {
+        return `You tied. You both chose ${playerSelection}`;
+    } 
+    // player chooses rock
+    else if (playerSelection == "rock") {
+        if (computerSelection == "scissors" ) {
+            return resultString("win", playerSelection, computerSelection);
+        } else {
+            return resultString("lose", computerSelection, playerSelection);
+        }
+    } 
+    // player chooses paper
+    else if (playerSelection == "paper") {
+        if (computerSelection == "rock" ) {
+            return resultString("win", playerSelection, computerSelection);
+        } else {
+            return resultString("lose", computerSelection, playerSelection);
+        }
+    } 
+    // player chooses scissors
+    else {
+        if (computerSelection == "paper" ) {
+            return resultString("win", playerSelection, computerSelection);
+        } else {
+            return resultString("lose", computerSelection, playerSelection);
+        }
+    } 
+    
+}
+
+function resultString(result, winner, loser) {
+    winnerProperCase = winner.substring(0,1).toUpperCase() + winner.substring(1).toLowerCase();
+    loserProperCase = loser.substring(0,1).toUpperCase() + loser.substring(1).toLowerCase();
+    resultProperCase = result.substring(0,1).toUpperCase() + result.substring(1).toLowerCase();
+
+    return `You ${resultProperCase}! ${winnerProperCase} beats ${loserProperCase}`;
+}
+
+
+
+
+
